ee9f313b8bd3c8b27c188d28a5db7cf1eadaa575
Anas Irfan <anasirfan101010@gmail.com>
2024-07-24 09:18:30 -0700

    finalize

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const header = document.getElementById("header");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 50) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
+    });
+
+   });
